292c4c0dd984ada2b7a20206af1b7fef1277b27d
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-06-17 19:48:01 +0100

    feat: add pulsing ring animation to audio capture button
    - Convert StartAudioCaptureWidget from StatelessWidget to StatefulWidget
    - Add animated pulsing rings with staggered phase offset
    - Enlarge mic button with shadow elevation and accent color

--- lib/widgets/start_audio_widget.dart
+++ lib/widgets/start_audio_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class StartAudioCaptureWidget extends StatelessWidget {
+class StartAudioCaptureWidget extends StatefulWidget {
   final String buttonText;
   final Function() onButtonPressed;
 
@@ -10,23 +10,109 @@ class StartAudioCaptureWidget extends StatelessWidget {
     required this.onButtonPressed,
   });
 
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      width: 100,
-      child: InkWell(
-        onTap: onButtonPressed,
-        customBorder: CircleBorder(),
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [BoxShadow(color: Colors.blue.withOpacity(0.9))],
-          ),
+  @override
+  State<StartAudioCaptureWidget> createState() =>
+      _StartAudioCaptureWidgetState();
+}
 
-          child: Icon(Icons.mic, color: Colors.white, size: 30),
-        ),
+class _StartAudioCaptureWidgetState extends State<StartAudioCaptureWidget>
+    with SingleTickerProviderStateMixin {
+  static const double _buttonSize = 200;
+  static const double _ringScale = 1.6;
+  static const Color _accent = Color.fromARGB(255, 2, 111, 201);
+
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2200),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final expandedSize = _buttonSize * _ringScale;
+    return SizedBox(
+      height: expandedSize,
+      width: expandedSize,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          _PulsingRing(animation: _controller, phase: 0.0, color: _accent),
+          _PulsingRing(animation: _controller, phase: 0.5, color: _accent),
+          SizedBox(
+            height: _buttonSize,
+            width: _buttonSize,
+            child: InkWell(
+              onTap: widget.onButtonPressed,
+              customBorder: const CircleBorder(),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _accent,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: const Icon(Icons.mic, color: Colors.white, size: 60),
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _PulsingRing extends StatelessWidget {
+  final Animation<double> animation;
+  final double phase;
+  final Color color;
+
+  const _PulsingRing({
+    required this.animation,
+    required this.phase,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        final t = (animation.value + phase) % 1.0;
+        final eased = Curves.easeOut.transform(t);
+        final scale = 1.0 + eased * 0.6;
+        final opacity = (1.0 - t).clamp(0.0, 1.0) * 0.7;
+        return Transform.scale(
+          scale: scale,
+          child: Opacity(
+            opacity: opacity,
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: color, width: 4),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
